a09af36c9e51219bacbe00a34efdb40138b98503
James.He <James.He@github.com>
2018-02-23 18:43:24 -0400

    firt commit

---
+++ js/personal.js
@@ -0,0 +1,14 @@
+
+var buttons = document.getElementsByTagName("Button");
+for(var i=0; i<buttons.length; i++){
+    buttons[i].addEventListener("click",function () {
+        alert("Still Constructing");
+    });
+}
+
+// var links = document.getElementsByTagName("a");
+// for(var i=0; i<links.length; i++){
+//     links[i].addEventListener("click",function () {
+//         alert("Still Constructing");
+//     });
+// }
